b002bd5ac75b6f54e53b5bc127c69156732dd716
Pablo <pablo.ledesma.p@gmail.com>
2015-11-01 16:33:03 -0500

    Backfill restaurantName

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem "rspec"
 gem "pry"
 gem 'sqlite3'
+gem 'pg'
--- test.rb
+++ test.rb
@@ -3,10 +3,20 @@ require "pg"
 require "pry"
 require "csv"
 
-
 db = SQLite3::Database.new 'restaurants.db'
 
-# Find some records
-db.execute 'SELECT * FROM purchases' do |row|
-  puts row
+db.execute("INSERT INTO purchase_data (value) VALUES (?)", @workplace)
+
+@workplace = nil
+
+# Select from purchase_data
+db.execute 'SELECT * FROM purchases JOIN purchase_data ON purchases.id = purchase_data.purchaseId' do |row|
+  if row[5] == "employeeId"
+    @purchaseId = row[1]
+    CSV.foreach('employees.csv', headers: true) do |csv_row|
+      @workplace =  csv_row[-1]
+    end
+  elsif row[5] == "restaurantName" && row[6].nil?
+    row[6] = @workplace
+  end
 end
